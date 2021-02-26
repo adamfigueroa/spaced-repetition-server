c244aa06a72ecb2a381a37057ab6ad08f1017414
Adam Figueroa <admiralfiggins@gmail.com>
2021-02-26 11:00:45 -0800

    added /head GET route

--- src/language/language-service.js
+++ src/language/language-service.js
@@ -7,10 +7,10 @@ const LanguageService = {
         'language.name',
         'language.user_id',
         'language.head',
-        'language.total_score',
+        'language.total_score'
       )
       .where('language.user_id', user_id)
-      .first()
+      .first();
   },
 
   getLanguageWords(db, language_id) {
@@ -24,10 +24,17 @@ const LanguageService = {
         'next',
         'memory_value',
         'correct_count',
-        'incorrect_count',
+        'incorrect_count'
       )
-      .where({ language_id })
+      .where({ language_id });
   },
-}
 
-module.exports = LanguageService
+  getNextWord(db, id) {
+    return db('word')
+      .select('id', 'next', 'original', 'correct_count', 'incorrect_count')
+      .where({ id })
+      .first();
+  },
+};
+
+module.exports = LanguageService;
